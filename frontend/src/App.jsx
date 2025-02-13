758307318606e622ac116f73862e4d453e416e63
Luka <manialuka19@gmail.com>
2025-02-13 17:26:01 +0400

    Define and test the prepared styles from my figma project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,26 @@
+import { useEffect } from "react"
+
 export default function App() {
-  return <h1 className="text-gray-500 text-3xl font-bold">Weather forecast</h1>
+  useEffect(_ => {
+    document.documentElement.setAttribute('data-theme', 'light')
+  }, [])
+
+  return (<>
+    <div className="p-4 bg-blue-50">
+      <p style={{fontSize: "var(--text-heading-primary)"}}>Heading 1</p>
+      <p style={{fontSize: "var(--text-heading-secondary)"}}>Heading 2</p>
+      <p style={{fontSize: "var(--text-body-primary)"}}>Body 1</p>
+      <p style={{fontSize: "var(--text-body-secondary)"}}>Body 1</p>
+    </div>
+
+    <div className="p-4 bg-black">
+      <p className="text-[var(--color-text-primary)]">#fff</p>
+      <p className="text-[var(--color-text-secondary)]">#e0e0e0</p>
+    </div>
+
+    <div className="p-4 bg-blue-50 flex gap-4">
+      <div className="test-box" style={{background: "var(--color-bg-primary)"}}></div>
+      <div className="test-box" style={{background: "var(--color-bg-secondary)"}}></div>
+    </div>
+  </>)
 }
